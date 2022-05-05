c54b49ddf3e5c4d465eebe4a77ecb17ffa7bdabf
Henrique Marques <henrique_marques.dev@outlook.com>
2022-05-05 19:53:36 -0300

    feat: widget component styled done

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,6 +1,19 @@
 import { FC } from "react";
 import { ChatTeardropDots } from "phosphor-react";
 
+import { Container } from "./styles";
+
 export const Widget: FC = () => {
-  return <button></button>;
+  return (
+    <Container>
+      <button>
+        <ChatTeardropDots className="icon" />
+
+        <span>
+          <span />
+          Feedback
+        </span>
+      </button>
+    </Container>
+  );
 };
--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,1 +1,50 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  position: absolute;
+  bottom: 1.25rem;
+  right: 1.25rem;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 3rem;
+
+    padding: 0.75rem;
+
+    background-color: ${({ theme }) => theme.colors.brand};
+    border: none;
+    border-radius: 1.75rem;
+
+    color: ${({ theme }) => theme.colors.textOnBrandColor};
+
+    .icon {
+      width: 1.5rem;
+      height: 1.5rem;
+    }
+
+    &:hover {
+      span {
+        max-width: 20rem;
+
+        transition: all ease 0.5s;
+      }
+    }
+
+    span {
+      overflow: hidden;
+
+      max-width: 0;
+
+      font-size: 1rem;
+
+      transition: all ease 0.5s;
+
+      span {
+        padding-left: 0.5rem;
+      }
+    }
+  }
+`;
